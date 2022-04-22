c9fbb843929a7d53bf138047bd965ea60e7d0a7b
Ciro Callazans <ccallazans@protonmail.com>
2022-04-22 14:58:23 -0300

    fix app.py

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 mkdir -p ~/.streamlit/echo "\
+[general]\n\
+email = \"ccallazans@protonmail.com\"\n\
+" > ~/.streamlit/credentials.tomlecho "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
